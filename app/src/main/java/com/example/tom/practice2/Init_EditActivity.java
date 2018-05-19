ec721961e00576a65f71379815a652ce0f342442
tomholb <37909938+tomholb@users.noreply.github.com>
2018-05-19 11:07:03 -0400

    Initial commit

---
+++ app/src/main/java/com/example/tom/practice2/Init_EditActivity.java
@@ -0,0 +1,13 @@
+package com.example.tom.practice2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Init_EditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_init_edit);
+    }
+}
